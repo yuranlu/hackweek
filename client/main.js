83a6ed9f7391f86095db87e6282118b044a99504
Yuran Lu <yuranlu@videable.com>
2013-08-05 23:03:20 -0700

    indented

--- client/main.js
+++ client/main.js
@@ -2,12 +2,12 @@ times = null;
 streamsource = null;
 audioContext = null;
 analyser = null;
-    var ts = new TimeSeries();
-    times = ts;
-    var ts2 = new TimeSeries();
-    times2 = ts2;
-     buflen = 1024;
-      buf = new Uint8Array( buflen );
+var ts = new TimeSeries();
+times = ts;
+var ts2 = new TimeSeries();
+times2 = ts2;
+buflen = 1024;
+buf = new Uint8Array( buflen );
 
 
 var cht = new SmoothieChart();
@@ -16,16 +16,18 @@ Meteor.defer( function () {
     cht.addTimeSeries(ts2, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 4 });
     cht.streamTo(document.getElementById("chart"), 20);
 });
+
 window.addEventListener('load', init, false);
+
 function init() {
-  try {
-    // Fix up for prefixing
-    window.AudioContext = window.AudioContext||window.webkitAudioContext;
-    audioContext = new AudioContext();
-  }
-  catch(e) {
-    alert('Web Audio API is not supported in this browser');
-  }
+    try {
+	// Fix up for prefixing
+	window.AudioContext = window.AudioContext||window.webkitAudioContext;
+	audioContext = new AudioContext();
+    }
+    catch(e) {
+	alert('Web Audio API is not supported in this browser');
+    }
 }
 
 function convertToMono( input ) {
@@ -39,75 +41,75 @@ function convertToMono( input ) {
 }
 
 (function (doc, nav) {
-  "use strict";
+    "use strict";
 
     var f = 0;
     Meteor.setInterval(function () {document.getElementById("f").innerHTML = f + " fps"; f = 0;}, 1000);
 
-  var div, audio, video, width, height, context;
-  var bufidx = 0, buffers = [];
+    var div, audio, video, width, height, context;
+    var bufidx = 0, buffers = [];
 
-  function initialize() {
-    // The source video.
-    audio = doc.getElementById("a");
-    video = doc.getElementById("v");
-    width = video.width;
-    height = video.height;
-      div = document.getElementById("d");
+    function initialize() {
+	// The source video.
+	audio = doc.getElementById("a");
+	video = doc.getElementById("v");
+	width = video.width;
+	height = video.height;
+	div = document.getElementById("d");
 
-    // The target canvas.
-    var canvas = doc.getElementById("c");
-    context = canvas.getContext("2d");
+	// The target canvas.
+	var canvas = doc.getElementById("c");
+	context = canvas.getContext("2d");
 
-    // Prepare buffers to store lightness data.
-    for (var i = 0; i < 2; i++) {
-      buffers.push(new Uint8Array(width * height));
+	// Prepare buffers to store lightness data.
+	for (var i = 0; i < 2; i++) {
+	    buffers.push(new Uint8Array(width * height));
+	}
+
+	// Get the webcam's stream.
+	nav.getUserMedia({video: true, audio: true}, startStream, function () {});
     }
 
-    // Get the webcam's stream.
-    nav.getUserMedia({video: true, audio: true}, startStream, function () {});
-  }
+    function startStream(stream) {
+	video.src = URL.createObjectURL(stream);
+	video.play();
+	video.muted = true;
 
-  function startStream(stream) {
-    video.src = URL.createObjectURL(stream);
-    video.play();
-      video.muted = true;
+	streamsource = audioContext.createMediaStreamSource(stream);
+	analyser = audioContext.createAnalyser();
+	analyser.fftSize = 2048;
+	convertToMono( streamsource ).connect( analyser );
+	
+	audio.src = URL.createObjectURL(stream);
+	audio.play();
+	audio.muted = true;
 
-      streamsource = audioContext.createMediaStreamSource(stream);
-      analyser = audioContext.createAnalyser();
-      analyser.fftSize = 2048;
-      convertToMono( streamsource ).connect( analyser );
-      
-    audio.src = URL.createObjectURL(stream);
-    audio.play();
-      audio.muted = true;
-
-    // Ready! Let's start drawing.
-    requestAnimationFrame(draw);
-  }
-
-  function draw() {
-    var frame = readFrame();
-
-    if (frame) {
-      markLightnessChanges(frame.data);
-      context.putImageData(frame, 0, 0);
+	// Ready! Let's start drawing.
+	requestAnimationFrame(draw);
     }
 
-    // Wait for the next frame.
-    requestAnimationFrame(draw);
-  }
+    function draw() {
+	var frame = readFrame();
 
-  function readFrame() {
-    try {
-      context.drawImage(video, 0, 0, width, height);
-    } catch (e) {
-      // The video may not be ready, yet.
-      return null;
+	if (frame) {
+	    markLightnessChanges(frame.data);
+	    context.putImageData(frame, 0, 0);
+	}
+
+	// Wait for the next frame.
+	requestAnimationFrame(draw);
     }
 
-    return context.getImageData(0, 0, width, height);
-  }
+    function readFrame() {
+	try {
+	    context.drawImage(video, 0, 0, width, height);
+	} catch (e) {
+	    // The video may not be ready, yet.
+	    return null;
+	}
+
+	return context.getImageData(0, 0, width, height);
+    }
 
     function markLightnessChanges(data) {
 	// Pick the next buffer (round-robin).
@@ -146,15 +148,15 @@ function convertToMono( input ) {
 	times.append(new Date().getTime(), nChanged);
     }
 
-  function lightnessHasChanged(index, value) {
-    return buffers.some(function (buffer) {
-      return Math.abs(value - buffer[index]) >= 15;
-    });
-  }
+    function lightnessHasChanged(index, value) {
+	return buffers.some(function (buffer) {
+	    return Math.abs(value - buffer[index]) >= 15;
+	});
+    }
 
-  function lightnessValue(r, g, b) {
-    return (Math.min(r, g, b) + Math.max(r, g, b)) / 255 * 50;
-  }
+    function lightnessValue(r, g, b) {
+	return (Math.min(r, g, b) + Math.max(r, g, b)) / 255 * 50;
+    }
 
-  addEventListener("DOMContentLoaded", initialize);
+    addEventListener("DOMContentLoaded", initialize);
 })(document, navigator);
