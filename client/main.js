910d50989756bce11fdc6747d04eb89e6da90224
Yuran Lu <yuranlu@videable.com>
2013-08-05 21:22:25 -0700

    THIS IS REALLY CRAPPY BUT WE GOTS SOUND AND VIDEO ANALYSES

--- client/main.js
+++ client/main.js
@@ -1,17 +1,48 @@
+times = null;
+streamsource = null;
+audioContext = null;
+analyser = null;
+    var ts = new TimeSeries();
+    times = ts;
+    var ts2 = new TimeSeries();
+    times2 = ts2;
+     buflen = 1024;
+      buf = new Uint8Array( buflen );
+
+
+var cht = new SmoothieChart();
+Meteor.defer( function () {
+    cht.addTimeSeries(ts, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+    cht.addTimeSeries(ts2, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.2)', lineWidth: 4 });
+    cht.streamTo(document.getElementById("chart"), 20);
+});
 window.addEventListener('load', init, false);
 function init() {
   try {
     // Fix up for prefixing
     window.AudioContext = window.AudioContext||window.webkitAudioContext;
-    context = new AudioContext();
+    audioContext = new AudioContext();
   }
   catch(e) {
     alert('Web Audio API is not supported in this browser');
   }
 }
 
+function convertToMono( input ) {
+    var splitter = audioContext.createChannelSplitter(2);
+    var merger = audioContext.createChannelMerger(2);
+
+    input.connect( splitter );
+    splitter.connect( merger, 0, 0 );
+    splitter.connect( merger, 0, 1 );
+    return merger;
+}
+
 (function (doc, nav) {
   "use strict";
+
+    var f = 0;
+    Meteor.setInterval(function () {document.getElementById("f").innerHTML = f + " fps"; f = 0;}, 1000);
 
   var div, audio, video, width, height, context;
   var bufidx = 0, buffers = [];
@@ -40,9 +71,16 @@ function init() {
   function startStream(stream) {
     video.src = URL.createObjectURL(stream);
     video.play();
+      video.muted = true;
 
+      streamsource = audioContext.createMediaStreamSource(stream);
+      analyser = audioContext.createAnalyser();
+      analyser.fftSize = 2048;
+      convertToMono( streamsource ).connect( analyser );
+      
     audio.src = URL.createObjectURL(stream);
     audio.play();
+      audio.muted = true;
 
     // Ready! Let's start drawing.
     requestAnimationFrame(draw);
@@ -71,32 +109,42 @@ function init() {
     return context.getImageData(0, 0, width, height);
   }
 
-  function markLightnessChanges(data) {
-    // Pick the next buffer (round-robin).
-    var buffer = buffers[bufidx++ % buffers.length];
-
+    function markLightnessChanges(data) {
+	// Pick the next buffer (round-robin).
+	var buffer = buffers[bufidx++ % buffers.length];
+	
 	var nChanged = 0;
-
-    for (var i = 0, j = 0; i < buffer.length; i++, j += 4) {
-      // Determine lightness value.
-      var current = lightnessValue(data[j], data[j + 1], data[j + 2]);
-
-      // Set color to black.
-      data[j] = data[j + 1] = data[j + 2] = 0;
-
-	var changed = lightnessHasChanged(i, current);
-	if (changed) 
-	    nChanged ++;
-
-      // Full opacity for changes.
-      data[j + 3] = 255 * lightnessHasChanged(i, current);
-
-      // Store current lightness value.
-      buffer[i] = current;
+	
+	for (var i = 0, j = 0; i < buffer.length; i++, j += 4) {
+	    // Determine lightness value.
+	    var current = lightnessValue(data[j], data[j + 1], data[j + 2]);
+	    
+	    // Set color to black.
+	    data[j] = data[j + 1] = data[j + 2] = 0;
+	    
+	    var changed = lightnessHasChanged(i, current);
+	    if (changed) 
+		nChanged ++;
+	    
+	    // Full opacity for changes.
+	    data[j + 3] = 255 * lightnessHasChanged(i, current);
+	    
+	    // Store current lightness value.
+	    buffer[i] = current;
+	}
+	f++;
+	div.innerHTML = nChanged;
+	analyser.getByteTimeDomainData( buf );
+	var size = 0;
+	for (var i=0; i<buflen; i++) {
+	    var foo = buf[i] - 128;
+	    if (foo < 0)
+		foo = -foo;
+	    size += foo;
+	}
+	times2.append(new Date().getTime(), size);
+	times.append(new Date().getTime(), nChanged);
     }
-
-      div.innerHTML = nChanged;
-  }
 
   function lightnessHasChanged(index, value) {
     return buffers.some(function (buffer) {
